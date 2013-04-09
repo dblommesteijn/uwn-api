669af7408979e5b140b8fe1b2f265f653a9a5195
Dennis Blommesteijn <dennis@blommesteijn.com>
2013-04-09 16:11:14 +0200

    Gemspec; UnitTests.

---
+++ test/unit/uwn_connect.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'test/unit'
+require File.expand_path(File.dirname(__FILE__) + "/../../lib/uwn/api")
+
+#
+#
+
+class UwnConnect < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_connection_uwn_offline
+
+  end
+
+  def test_connection_uwn_online
+  end
+
+
+  def test_connect_subject_uris
+  end
+
+
+end
--- uwn-api.gemspec
+++ uwn-api.gemspec
@@ -20,4 +20,9 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+
+  # dependencies
+  spec.add_dependency 'nokogiri', "~> 1.5.0"
+  spec.add_dependency 'json', "~> 1.6.1"
+  spec.add_development_dependency "test-unit", "~> 2.0.0"
 end
